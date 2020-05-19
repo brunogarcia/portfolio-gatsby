68323fd7875badac9ba4fd0e5e4cccfc6631bc57
Bruno Garcia <bruno@garciaechegaray.com>
2020-05-19 18:01:27 +0200

    Add avatar to the homepage

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -19,17 +19,25 @@ export default () => (
     `}
     render={data => (
       <div className="hero-header">
-        <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
-        />
-        <a href='https://github.com/brunogarcia' className="button -primary">
-          <GithubIcon role="img" aria-label="github icon" /> Github
-        </a>
-        <a href='https://www.linkedin.com/in/bruno-garcia-echegaray' className="button -primary">
-          <LinkedinIcon role="img" aria-label="Linkedin icon" /> Linkedin
-        </a>
+        <div className="row">
+          <div className="column-left">
+            <img src="/assets/bruno-garcia.jpg" alt="Avatar of Bruno" />
+          </div>
+          <div className="column-right">
+            <div className="headline">
+              {data.site.siteMetadata.home.title}
+            </div>
+            <div className="primary-content">
+              { data.site.siteMetadata.home.description }
+            </div>
+            <a href='https://github.com/brunogarcia' className="button -primary">
+              <GithubIcon role="img" aria-label="github icon" /> Github
+            </a>
+            <a href='https://www.linkedin.com/in/bruno-garcia-echegaray' className="button -primary">
+              <LinkedinIcon role="img" aria-label="Linkedin icon" /> Linkedin
+            </a>
+          </div>
+          </div>
       </div>
     )}
   />
